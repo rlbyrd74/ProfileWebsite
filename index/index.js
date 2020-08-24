3858b7b8d033fac224b93405d293e639f1861a00
The Sharp Ninja <ninja@thesharp.ninja>
2020-08-23 19:51:32 -0500

    Initial commit

---
+++ index/index.js
@@ -0,0 +1,12 @@
+﻿if (navigator.platform.indexOf("Win") != -1)
+{
+$(".windows-show").show();
+}
+if (navigator.platform.indexOf("Mac") != -1)
+{
+$(".mac-show").show();
+}
+if (navigator.platform.indexOf("Mac")==-1 && navigator.platform.indexOf("Win") ==-1)
+{
+$(".others-show").show();
+}
